4cf6f4edab4e99a4cdaa571efc062bb6b4f77b55
Sucheta Sheshadri <264160564+suchetasheshadri@users.noreply.github.com>
2026-08-12 19:26:41 +0530

    Add testbench for NOR gate

---
+++ basic-gates/nor_gate_tb.v
@@ -0,0 +1,26 @@
+module norgate_tb;
+  reg a_tb;
+  reg b_tb;
+  wire y_tb;
+  
+  nor_gate dut(
+    .a(a_tb),
+    .b(b_tb),
+    .y(y_tb)
+  );
+  
+  initial begin
+    
+    a_tb=0; b_tb=0;
+    #10; $display("A=%0d, B=%0d, Y=%0d", a_tb, b_tb, y_tb);
+     a_tb=0; b_tb=1;
+    #10; $display("A=%0d, B=%0d, Y=%0d", a_tb, b_tb, y_tb);
+     a_tb=1; b_tb=0;
+    #10; $display("A=%0d, B=%0d, Y=%0d", a_tb, b_tb, y_tb);
+     a_tb=1; b_tb=1;
+    #10; $display("A=%0d, B=%0d, Y=%0d", a_tb, b_tb, y_tb);
+    
+    #60; $finish;
+    
+  end
+endmodule
